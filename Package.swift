46651b68276cc3d9a7da463a722595fb1c468b27
oldtimerhippie <86533748+oldtimerhippie@users.noreply.github.com>
2024-08-18 11:05:59 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,9 +24,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-       // .target(
-       //     name: "FLACMetadataKit",
-       //     dependencies: ["Alamofire"]),
+        .target(
+            name: "FLACMetadataKit",
+            dependencies: []),
         .target(
             name: "TestCommon",
             dependencies: ["FLACMetadataKit"],
